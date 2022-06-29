a4af72d315ddd3a6e28db4c952853cfa5871411e
Tobias Schlitt <toby@frontastic.cloud>
2022-06-29 12:21:15 +0200

    feat(FP-2185): Type hint for Adyen extension registry.

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import * as PaymentActions from './actionControllers';
+import { ExtensionRegistry } from '@frontastic/extension-types';
 
 export default {
   actions: {
     payment: PaymentActions,
   },
-};
+} as ExtensionRegistry;
