d0dd6db2378136b9732b83155eb6df1b5accce1b
Carlos Mourullo <carlosmourullo@gmail.com>
2022-07-01 19:13:54 +0200

    fix: extracted Amount from adyen session and use it in components

--- Session.ts
+++ Session.ts
@@ -1,7 +1,5 @@
-export interface Amount {
-  currency: string; // The currency code compliant to ISO 4217.
-  value: number; // In cents
-}
+import {Amount} from "@Types/Amount";
+
 export interface CreateSessionDTO {
   amount: Amount;
   returnUrl: string;
